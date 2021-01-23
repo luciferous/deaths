495bd6b84d2d469250118999019ebca56769607b
Neuman Vong <neuman.vong@gmail.com>
2021-01-23 17:15:53 +0800

    AU: use the provisional doctor-certified deaths
    The previous version was comparing two datasets that can't accurately be
    compared. The pre-2020 dataset contained every death registered, but the 2020
    dataset only contained doctor-certified deaths, which is why the 2020 counts
    were comparatively low.
    
    Doctor-certified deaths are registered within days or weeks, while
    coroner-certified deaths can be delayed for weeks or months.
    
    This change makes it so that we're using only doctor-certified death datasets.
    When the death data for 2020 is complete, then this might be worth revisiting
    so that we can get a picture of all deaths occurred.

--- au/README.md
+++ au/README.md
@@ -1,36 +1,36 @@
+# Australia
+
+
+These datasets contain only the doctor-certified deaths (i.e.,
+coroner-certified deaths are missing).
 
 Go to
 https://www.abs.gov.au/statistics/health/causes-death/provisional-mortality-statistics/latest-release#data-download
-and download the "Provisional mortality statistics weekly dashboard", or run:
+and download
+
+- "Provisional mortality statistics weekly dashboard"
+- "Doctor certified deaths by week of occurrence, 2015-19"
+
+Or run:
 
 ```
-wget https://www.abs.gov.au/statistics/health/causes-death/provisional-mortality-statistics/jan-oct-2020/Provisional%20Mortality%20Statistics%2C%20Weekly%20Dashboard%2C%20Jan-Oct%202020%20with%20SDRs.xlsx
-```
-
-Convert the spreadsheet to a CSV.
-
-```
-./au-to-csv.R Provisional\ Mortality\ Statistics,\ Weekly\ Dashboard,\ Jan-Oct\ 2020\ with\ SDRs.xlsx Table\ 1.1 > Provisional\ Mortality\ Statistics,\ Weekly\ Dashboard,\ Jan-Oct\ 2020\ with\ SDRs.csv
-```
-
-Go to https://www.bdm.vic.gov.au/research-and-family-history/research-and-data-services/death-statistics/deaths-registered-per-month and download "Death registrations by month", or run:
-
-``` 
-wget https://www.bdm.vic.gov.au/sites/default/files/embridge_cache/emshare/original/public/2021/01/48/2b7cc4d8d/Death%20Registrations%20by%20Month.XLSX
+$ wget https://www.abs.gov.au/statistics/health/causes-death/provisional-mortality-statistics/jan-oct-2020/Provisional%20Mortality%20Statistics%2C%20Weekly%20Dashboard%2C%20Jan-Oct%202020%20with%20SDRs.xlsx
+$ wget https://www.abs.gov.au/statistics/health/causes-death/provisional-mortality-statistics/jan-oct-2020/Doctor%20certified%20deaths%20by%20week%20of%20occurrence%2C%202015-19.xlsx
 ```
 
 Convert the spreadsheet to a CSV.
 
 ```
-./au-to-csv.R Death\ Registrations\ by\ Month.XLSX Sheet1 > Death\ Registrations\ by\ Month.csv
+$ ./au-to-csv.R Provisional\ Mortality\ Statistics,\ Weekly\ Dashboard,\ Jan-Oct\ 2020\ with\ SDRs.xlsx Table\ 1.1 > Provisional\ Mortality\ Statistics,\ Weekly\ Dashboard,\ Jan-Oct\ 2020\ with\ SDRs.csv
+$ ./au-to-csv.R Doctor\ certified\ deaths\ by\ week\ of\ occurrence\,\ 2015-19.xlsx Table\ 2.3 > Doctor\ certified\ deaths\ by\ week\ of\ occurrence\,\ 2015-19.csv
 ```
 
-Compile and format the CSVs into one CSV.
+Compile and format the CSVs.
 
 ```
 ./main > au-deaths.csv
 ```
 
-Update the spreadsheet at
-https://docs.google.com/spreadsheets/d/1eSaKBxUfWB1payf2OjEZt5x83CaeK_RIuq8u1heji9E/edit#gid=1434566235,
-replacing the sheet.
+Update the
+[spreadsheet](https://docs.google.com/spreadsheets/d/1eSaKBxUfWB1payf2OjEZt5x83CaeK_RIuq8u1heji9E/edit#gid=1434566235),
+replacing it.
--- au/main.R
+++ au/main.R
@@ -1,59 +1,54 @@
 #!/usr/bin/env Rscript
 
-library(plyr)
 library(zoo)
 
-### ABS.Stat final historical.
-final <- read.csv("DEATHS_MONTHOCCURENCE_17012021043159486.csv")
-final <- final[final$MONTHS != "TOT",]
-final$Date <- as.yearmon(paste(final$Time, final$MONTHS, sep="-"))
-final <- subset(final,
-               Measure == "All Deaths" &
-                 Sex == "Persons" &
-                 Region != "Australia",
-               select=c(Date, Region, Value))
-final <- reshape(final, idvar="Date", timevar="Region", direction="wide")
-names(final) <- sub("Value.", "", names(final))
-final.zoo <- read.zoo(final)
-
-# Combine NT and ACT because that's what the provisional data does.
-final.zoo$`Northern Territory and Australian Capital Territory` <-
-  final.zoo$`Northern Territory` + final.zoo$`Australian Capital Territory`
-final.zoo <- subset(final.zoo, select=-c(`Northern Territory`, `Australian Capital Territory`))
-
-### Victoria 2019-2020.
-vic <- read.csv("Death Registrations by Month.csv") 
-colnames(vic) <- c("Date", "Victoria")
-vic.zoo <- read.zoo(vic, drop=F)
-time(vic.zoo) <- as.yearmon(time(vic.zoo))
-vic.final <- subset(final.zoo, select=Victoria)
-final.zoo <- subset(final.zoo, select=-Victoria)
-vic.final <- window(vic.final, end=start(vic.zoo) - 1/12)
-vic.complete <- c(vic.final, vic.zoo)
-
-### ABS provisional 2020.
-path <- "Provisional Mortality Statistics, Weekly Dashboard, Jan-Oct 2020 with SDRs.csv"
-data <- read.csv(path, header=F)
-rows <- c(
-  "New South Wales - 2020",
-  "Northern Territory and Australian Capital Territory - 2020",
-  "Queensland - 2020",
-  "South Australia - 2020",
-  "Tasmania - 2020",
-  #"Victoria - 2020",
-  "Western Australia - 2020"
+states <- c(
+  "New South Wales",
+  "Northern Territory and Australian Capital Territory",
+  "Queensland",
+  "South Australia",
+  "Tasmania",
+  "Victoria",
+  "Western Australia"
 )
 
-data <- data[data$V1 %in% rows,]
-data <- as.data.frame(t(data))
-colnames(data) <- sub(" - 2020$", "", data[1,])
-data <- data[-1,]
-data <- as.data.frame(sapply(as.data.frame(data), as.numeric))
-data$Date <- as.yearmon(paste("2020", index(data), "1"), "%Y %U %u")
-data <- aggregate(. ~ Date, data, sum)
-data.zoo <- read.zoo(data)
+normalized <- data.frame()
 
-complete <- rbind(final.zoo, data.zoo)
-complete <- merge(complete, vic.complete)
+path <- "Doctor certified deaths by week of occurrence, 2015-19.csv"
+data <- read.csv(path, header=F)
+
+for (state in states) {
+  ix <- which(data$V1 == state)
+  rows <- data[(ix+1):(ix+5),]
+  rows$V1 <- paste(state, rows$V1, sep=" - ")
+  normalized <- rbind(normalized, rows)
+}
+
+path <- "Provisional Mortality Statistics, Weekly Dashboard, Jan-Oct 2020 with SDRs.csv"
+data <- read.csv(path, header=F)
+data <- data[data$V1 %in% paste(states, "2020", sep=" - "),]
+if (ncol(data) < ncol(normalized)) data[, (ncol(data)+1):ncol(normalized)] <- NA
+normalized <- rbind(normalized, data)
+
+complete <- data.frame()
+for (year in 2015:2020) {
+  data <- normalized[normalized$V1 %in% paste(states, year, sep=" - "),]
+  row.names(data) <- sub(paste(" - ", year, "$", sep=""), "", data$V1)
+  data <- subset(data, select=-V1)
+  data[] <- lapply(data, as.numeric)
+
+  ym <- if (ncol(data) <= 52) {
+    as.yearmon(paste(year, 1:ncol(data), 7), "%Y %U %u")
+  } else {
+    c(as.yearmon(paste(year, 1:52, 7), "%Y %U %u"),
+      as.yearmon(paste(year, 12, sep="-")))
+  }
+  data <- rbind(data, Date=ym)
+  data <- aggregate(. ~ Date, as.data.frame(t(data)), sum)
+  data$Date <- as.yearmon(data$Date)
+  complete <- rbind(complete, data)
+}
+
+complete <- read.zoo(complete)
 
 write.zoo(complete, index.name="Date", sep=",", na="")
